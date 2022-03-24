11b5856cf23d5153708679b6862dc2a40c732fd1
Avalynn Judge <avalynnjudge@gmail.com>
2022-03-24 15:06:03 -0700

    Added Service Worker

---
+++ client/webpack.config.js
@@ -0,0 +1,87 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      
+      // this might be wrong
+      editor: './src/js/editor.js',
+
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
+    plugins: [
+      // webpack plugin which generates our html file and injects the bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TextPop',
+      }),
+
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
+
+      // injects the custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'TextPop',
+        short_name: 'TextPop',
+        description: 'take notes on the web or pop the window out to use the app!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+
+    ],
+
+    // add CSS loaders and babel to webpack.
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
